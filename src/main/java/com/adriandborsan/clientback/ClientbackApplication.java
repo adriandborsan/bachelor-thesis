8ecbb26d6de7a2a87843be88d940c545456f58a8
grayson <adriandborsan@gmail.com>
2023-05-17 16:27:51 +0300

    Add initial empty project

---
+++ src/main/java/com/adriandborsan/clientback/ClientbackApplication.java
@@ -0,0 +1,13 @@
+package com.adriandborsan.clientback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientbackApplication.class, args);
+	}
+
+}
