60869a9b697aa4c321f626476e17136c858ae1f7
grayson <adriandborsan@gmail.com>
2023-05-30 15:14:44 +0300

    Add basic security

---
+++ src/main/java/com/adriandborsan/clientback/security/JwtConfig.java
@@ -0,0 +1,27 @@
+package com.adriandborsan.clientback.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+public class JwtConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuerUri;
+
+
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+
+        jwtDecoder.setJwtValidator(token -> OAuth2TokenValidatorResult.success());
+
+        return jwtDecoder;
+    }
+}
