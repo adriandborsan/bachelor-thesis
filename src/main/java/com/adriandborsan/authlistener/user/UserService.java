1a40eee39fca19eeca5250546b85cfbda1e59d34
grayson <adriandborsan@gmail.com>
2023-06-25 17:36:24 +0300

    Add docker and skaffold

--- src/main/java/com/adriandborsan/authlistener/user/UserService.java
+++ src/main/java/com/adriandborsan/authlistener/user/UserService.java
@@ -1,17 +1,19 @@
 package com.adriandborsan.authlistener.user;
 
+import com.adriandborsan.authlistener.neo4j.UserNode;
+import com.adriandborsan.authlistener.neo4j.UserNodeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserNodeRepository userNodeRepository;
 
     public <S extends UserEntity> S save(S entity) {
+        userNodeRepository.save(new UserNode(entity.getId()));
         return userRepository.save(entity);
     }
 }
