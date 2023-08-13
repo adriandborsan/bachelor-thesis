c91237369fa1e0f3255ea5b6a5707f1ee891b907
adriandborsan <adriandborsan@gmail.com>
2023-08-13 16:02:12 +0300

    Add comments to improve clarity for complex classes

--- src/app/posts/post-read/post-read.component.ts
+++ src/app/posts/post-read/post-read.component.ts
@@ -8,87 +8,114 @@ import { PostService } from '../post.service';
   styleUrls: ['./post-read.component.scss']
 })
 export class PostReadComponent implements OnInit {
+  // Input property to allow parent components to pass a specific userId. If null, fetches general posts.
   @Input() userId: string | null = null;
+
+  // Reference to the sentinel DOM element which helps determine when to fetch more posts.
   @ViewChildren('sentinel', { read: ViewContainerRef }) sentinel!: QueryList<ViewContainerRef>;
+
+  // Array to store posts that are fetched.
   posts: Post[] = [];
+  // Variable to track pagination.
   page: number = 0;
+  // Flag to track if posts are currently being fetched.
   loading: boolean = false;
+  // Instance of Intersection Observer to detect when sentinel is in view.
   observer!: IntersectionObserver;
+  // Flag to indicate if all available posts have been fetched.
   noMorePosts: boolean = false;
 
   constructor(private postService: PostService, private cdRef: ChangeDetectorRef) {
+    // Subscribe to postsModifiedEventEmitter to refresh data when posts are updated.
     this.postService.postsModifiedEventEmitter.subscribe(() => this.refreshData());
   }
 
+  // Lifecycle hook to fetch initial set of posts.
   ngOnInit(): void {
     this.refreshData();
   }
 
+  // Lifecycle hook to initialize Intersection Observer once the view is ready.
   ngAfterViewInit(): void {
+    // Watch for changes in the sentinel elements.
     this.sentinel.changes.subscribe(() => {
+      // If sentinel is present, initialize the Intersection Observer.
       if (this.sentinel.first) {
         this.initializeObserver();
       }
     });
 
-    // If the sentinel is already present, initialize the observer
+    // In case the sentinel is already in the view when the component is initialized.
     if (this.sentinel.first) {
       this.initializeObserver();
     }
   }
 
+  // Method to set up the Intersection Observer.
   initializeObserver(): void {
     setTimeout(() => {
+      // Ensure sentinel is still present.
       if (this.sentinel.first) {
-
+        // Initialize Intersection Observer to watch the sentinel.
         this.observer = new IntersectionObserver((entries) => {
-          if (entries[0].isIntersecting && !this.loading&& !this.noMorePosts) {
+          // Load more data if sentinel is in view, not currently loading, and there are more posts to fetch.
+          if (entries[0].isIntersecting && !this.loading && !this.noMorePosts) {
             this.loadMoreData();
           }
         });
+        // Start observing the sentinel.
         this.observer.observe(this.sentinel.first.element.nativeElement);
       }
-    }, 1000);
+    }, 1000); // Delay of 1 second to allow other potential DOM changes to settle.
   }
 
-
-
+  // Method to reset and fetch the initial set of posts.
   refreshData(): void {
-    this.page = 0; // reset the page number
+    // Reset page number.
+    this.page = 0;
+    // Reset noMorePosts flag.
     this.noMorePosts = false;
 
+    // Depending on whether a userId is provided, fetch user-specific posts or general posts.
     const posts$ = this.userId
       ? this.postService.getPostsByUser(this.userId, this.page, 5, [{property: 'id', direction: 'desc'}, {property: 'title', direction: 'desc'}])
       : this.postService.getPosts(this.page, 5, [{property: 'id', direction: 'desc'}, {property: 'title', direction: 'desc'}]);
 
+    // Subscribe to the Observable and update the posts array.
     posts$.subscribe(postResponse => {
       this.posts = postResponse.content;
       this.page++;
     });
   }
 
+  // Method to fetch more posts when sentinel is in view.
   async loadMoreData(): Promise<void> {
+    // Set loading to true.
     this.loading = true;
+
+    // Depending on whether a userId is provided, fetch user-specific posts or general posts.
     const posts$ = this.userId
-    ? this.postService.getPostsByUser(this.userId, this.page, 5, [{property: 'id', direction: 'desc'}, {property: 'title', direction: 'desc'}])
-    : this.postService.getPosts(this.page, 5, [{property: 'id', direction: 'desc'}, {property: 'title', direction: 'desc'}]);
+      ? this.postService.getPostsByUser(this.userId, this.page, 5, [{property: 'id', direction: 'desc'}, {property: 'title', direction: 'desc'}])
+      : this.postService.getPosts(this.page, 5, [{property: 'id', direction: 'desc'}, {property: 'title', direction: 'desc'}]);
+
+    // Subscribe to the Observable.
     posts$.subscribe(async postResponse => {
+      // If no more posts are returned, update flags.
       if (postResponse.content.length === 0) {
-        // There are no more posts to load
         this.loading = false;
         this.noMorePosts = true;
-        if (this.observer) {
-        }
       } else {
+        // Update the posts array by appending the new posts.
         this.posts = [...this.posts, ...postResponse.content];
         this.page++;
         this.loading = false;
 
-        // Trigger change detection and wait for the DOM to update
+        // Manually trigger Angular's change detection.
         this.cdRef.detectChanges();
+        // Wait for the DOM to update.
         await new Promise(resolve => setTimeout(resolve, 0));
 
-        // Check if the sentinel is still visible
+        // If the sentinel is still in view after loading more posts, fetch again.
         const sentinelRect = this.sentinel.first.element.nativeElement.getBoundingClientRect();
         if (sentinelRect.top <= window.innerHeight && sentinelRect.bottom >= 0 && !this.loading) {
           this.loadMoreData();
--- src/app/user-profile/user-edit-dialog/user-edit-dialog.component.ts
+++ src/app/user-profile/user-edit-dialog/user-edit-dialog.component.ts
@@ -12,34 +12,48 @@ import { lastValueFrom } from 'rxjs';
 })
 export class UserEditDialogComponent implements OnInit {
 
+  // Form group for the user edit form.
   editUserForm!: FormGroup;
+
+  // Variable to store and display a preview of the profile picture.
   profilePicturePreview: string | ArrayBuffer = '';
 
   constructor(
     private fb: FormBuilder,
+
+    // MatDialogRef provides a handle on the opened dialog. It can be used to close the dialog.
     public dialogRef: MatDialogRef<UserEditDialogComponent>,
+
+    // MAT_DIALOG_DATA is a constant used to access data passed to the dialog. In this case, user details.
     @Inject(MAT_DIALOG_DATA) public data: UserEntity,
+
+    // Service for user related operations, e.g., fetch or update user details.
     private userService: UserService
   ) {}
 
+  // Lifecycle hook that is called after data-bound properties are initialized.
   ngOnInit(): void {
+    // Initialize the form with the user data passed to the dialog.
     this.initializeForm(this.data);
   }
 
+  // Sets up the form group with controls for each user property.
   initializeForm(user: Partial<UserEntity> = {}) {
     this.editUserForm = this.fb.group({
-      displayUsername: [user.displayUsername || null, Validators.required],
-      firstName: [user.firstName || null, Validators.required],
-      lastName: [user.lastName || null, Validators.required],
-      bio: [user.bio || null],
-      profilePictureFile: [null]  // This will be a File
+      displayUsername: [user.displayUsername || null, Validators.required], // Required username control.
+      firstName: [user.firstName || null, Validators.required],             // Required first name control.
+      lastName: [user.lastName || null, Validators.required],               // Required last name control.
+      bio: [user.bio || null],                                              // Optional bio control.
+      profilePictureFile: [null]  // Placeholder for the profile picture file input.
     });
 
+    // If there's a profile picture in the provided user data, set it to the preview variable.
     if (user.profilePicture) {
       this.profilePicturePreview = user.profilePicture;
     }
   }
 
+  // Fetches user data using its ID and updates the form with the fetched data.
   fetchUser(): void {
     this.userService.getUser(this.data.id)
       .subscribe(user => {
@@ -47,34 +61,49 @@ export class UserEditDialogComponent implements OnInit {
       });
   }
 
+  // Triggered when a new profile picture is selected.
   handleFileInput(event: Event) {
+    // Get the selected file.
     const fileItem = (event.target as HTMLInputElement).files?.item(0);
+
+    // FileReader is used to read the contents of the file.
     const reader = new FileReader();
+
     reader.onload = (e: any) => {
+      // When file is read, set the result as the profile picture preview.
       if (e.target) {
         this.profilePicturePreview = e.target.result as string;
       }
     };
 
     if (fileItem) {
+      // Start reading the file as Data URL so it can be previewed.
       reader.readAsDataURL(fileItem);
+
+      // Update the form control value with the file.
       this.editUserForm.patchValue({
         profilePictureFile: fileItem
       });
     }
   }
 
+  // Triggered when the edit form is submitted.
   async onSubmit() {
     try {
+      // Attempt to update the user with the form data.
       const response = await lastValueFrom(this.userService.updateUser(this.data.id, this.editUserForm.value as Partial<UserEntity>));
+
+      // If successful, close the dialog.
       this.dialogRef.close();
     } catch (error) {
+      // Log any error that occurs.
       console.error(error);
     }
   }
 
 }
 
+// TypeScript interface that describes the shape of the form values. Helps with strong typing.
 interface FormValues {
   displayUsername: string | null;
   firstName: string | null;
